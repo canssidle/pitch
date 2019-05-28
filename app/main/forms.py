d7d3b47d9ae7d05f04e6e385721a5002696ec11d
canssidle <canssidlewairimu@gmail.com>
2019-05-28 09:54:43 +0300

    create the pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
-# class PitchForm(FlaskForm):
+class PitchForm(FlaskForm):
 
-#     title = StringField('Enter the pitch category', validators = [Required()])
-#     description = TextAreaField('Pitch description')
-#     submit = SubmitField('Submit')
+    title = StringField('Enter the pitch category', validators = [Required()])
+    description = TextAreaField('Pitch description')
+    submit = SubmitField('Submit')
 
 # class CommentForm(FlaskForm):
 #     name = TextAreaField('Write your comment here',validators = [Required()])
