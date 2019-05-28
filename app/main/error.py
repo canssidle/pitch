029f9e31a954e6d516d0538447dde422869008e4
canssidle <canssidlewairimu@gmail.com>
2019-05-28 09:53:25 +0300

    create the error page

--- app/main/error.py
+++ app/main/error.py
@@ -1,7 +1,7 @@
-# from flask import render_template
-# from . import main
+from flask import render_template
+from . import main
 
-# @main.app_errorhandler(404)
-# def four_Ow_four(error):
-#      return render_template('fourOwfour.html'),404
+@main.app_errorhandler(404)
+def four_Ow_four(error):
+     return render_template('fourOwfour.html'),404
      
