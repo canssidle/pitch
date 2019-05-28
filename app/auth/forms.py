699962ad72c5a367cbd7e3318fa2bd0d27719c74
canssidle <canssidlewairimu@gmail.com>
2019-05-28 10:41:17 +0300

    import in the forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,8 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,PasswordField,BooleanField,SubmitField
-# from wtforms.validators import Required,Email,EqualTo
-# from ..models import User
-# from wtforms import ValidationError
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
 
 # class LoginForm(FlaskForm):
 #     email = StringField('Your Email Address',validators=[Required(),Email()])
