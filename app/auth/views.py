c0fb4411f70204f67b6c5d2e38804d5445ed4683
canssidle <canssidlewairimu@gmail.com>
2019-05-28 10:44:33 +0300

    import in the views for auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,35 +6,35 @@ from .forms import LoginForm,RegistrationForm
 from .. import db
 from ..email import mail_message
 
-# @auth.route('/login',methods=['GET','POST'])
-# def login():
-#     login_form = LoginForm()
-#     if login_form.validate_on_submit():
-#         user = User.query.filter_by(email = login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect(request.args.get('next') or url_for('main.index'))
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-#         flash('Invalid username or Password')
+        flash('Invalid username or Password')
 
-#     title = "pitch login"
-#     return render_template('auth/login.html',login_form = login_form,title=title)
+    title = "pitch login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     flash('You have been successfully logged out')
-#     return redirect(url_for("main.index"))
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
 
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         mail_message("Welcome to One-Minute-Pitch","email/welcome_user",user.email, user=user)
-#         return redirect(url_for('auth.login'))
-#         title = "New Account"
-#     return render_template('auth/register.html',registration_form = form)
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message("Welcome to One-Minute-Pitch","email/welcome_user",user.email, user=user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
