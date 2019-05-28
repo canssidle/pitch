04d4d02eb94b653fc5f22ca0ad30f087a8bd596c
canssidle <canssidlewairimu@gmail.com>
2019-05-28 10:42:29 +0300

    creating the register form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,12 +10,12 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
 
-# class RegistrationForm(FlaskForm):
-#     email = StringField("Your Email Address",validators=[Required(),Email()])
-#     username = StringField("Enter your username",validators=[Required()])
-#     password = PasswordField("Password",validators=[Required(),EqualTo("password_confirm",message="Passwords must match")])
-#     password_confirm = PasswordField("Confirm Password",validators=[Required()])
-#     submit = SubmitField("Sign Up")
+class RegistrationForm(FlaskForm):
+    email = StringField("Your Email Address",validators=[Required(),Email()])
+    username = StringField("Enter your username",validators=[Required()])
+    password = PasswordField("Password",validators=[Required(),EqualTo("password_confirm",message="Passwords must match")])
+    password_confirm = PasswordField("Confirm Password",validators=[Required()])
+    submit = SubmitField("Sign Up")
 
 #     def validate_email(self,data_field):
 #         if User.query.filter_by(email = data_field.data).first():
