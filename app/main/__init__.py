cb4f0d4dd62e1d378e4f01bf2ea25a003b9709e1
canssidle <canssidlewairimu@gmail.com>
2019-05-28 09:52:49 +0300

    initialize the blueprints in the main

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,4 @@
-# from flask import Blueprint
-# main = Blueprint('main', __name__)
-# from . import views, error,forms
-# s.
+from flask import Blueprint
+main = Blueprint('main', __name__)
+from . import views, error,forms
+s.
