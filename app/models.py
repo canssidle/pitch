0f9cd51b191fec0318f355c2d239950c5a5c36bd
canssidle <canssidlewairimu@gmail.com>
2019-05-28 10:16:02 +0300

    creating the user class

--- app/models.py
+++ app/models.py
@@ -10,15 +10,15 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-# class User(UserMixin,db.Model):
-#     __tablename__= 'users'
-#     id=db.Column(db.Integer,primary_key =True)
-#     username = db.Column(db.String(255))
-#     email=db.Column(db.String(255),unique=True,index = True)
-#     bio = db.Column(db.String(255))
-#     profile_pic_path = db.Column(db.String())
-#     pass_hash =db.Column(db.String(255))
-#     pitches= db.relationship('Pitch',backref='user',lazy="dynamic")
+class User(UserMixin,db.Model):
+    __tablename__= 'users'
+    id=db.Column(db.Integer,primary_key =True)
+    username = db.Column(db.String(255))
+    email=db.Column(db.String(255),unique=True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_hash =db.Column(db.String(255))
+    pitches= db.relationship('Pitch',backref='user',lazy="dynamic")
 
 
 #     @property
