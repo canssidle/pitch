3978908f9399380485cc3b16a214e0f5e69f5229
canssidle <canssidlewairimu@gmail.com>
2019-05-28 10:05:24 +0300

    update profile

--- app/main/views.py
+++ app/main/views.py
@@ -104,20 +104,25 @@ def profile(uname):
 @login_required
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
+    if user is None:@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
+        abort(404)
 
-#     form = UpdateProfile()
+    form = UpdateProfile()
 
-#     if form.validate_on_submit():
-#         user.bio = form.bio.data
+    if form.validate_on_submit():
+        user.bio = form.bio.data
 
-#         db.session.add(user)
-#         db.session.commit()
+        db.session.add(user)
+        db.session.commit()
 
-#         return redirect(url_for('.profile',uname=user.username))
+        return redirect(url_for('.profile',uname=user.username))
 
-#     return render_template('profile/update.html',form =form)
+    return render_template('profile/update.html',form =form)
 
 # @main.route('/user/<uname>/update/pic',methods= ['POST'])
 # @login_required
