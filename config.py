cce57f826f0a830408f5828a138d0dc0e1fc9358
canssidle <canssidlewairimu@gmail.com>
2019-05-28 10:25:04 +0300

    create all the configurations in the config

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
-# import os
-# class Config:
-#     '''
-#     General configuration parent class
-#     '''
-#     SECRET_KEY = 'canssy'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     UPLOADED_PHOTOS_DEST='app/static/photos'
-#     #email configurations
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = 'canssy'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    #email configurations
 
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-#     SIMPLEMDE_JS_IIFE = True
-#     SIMPLEMDE_USE_CDN = True
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://canssidle:judycharles@localhost/pitches'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://canssidle:judycharles@localhost/pitches'
 
   
 
